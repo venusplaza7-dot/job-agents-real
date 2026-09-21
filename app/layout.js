85973a9ed7fb83c9057bc1d86ea31a3521544cd0
venusplaza7-dot <venusplaza7@gmail.com>
2026-09-21 18:21:36 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,1 @@
-export default function RootLayout(props) {
-  return (
-    <html lang="en">
-      <body>{props.children}</body>
-    </html>
-  );
-}
+export default function RootLayout({ children }) { return <html><body>{children}</body></html>; }
